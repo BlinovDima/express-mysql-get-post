c7abed23adf8136763d6a2b97bdd0439fcd83d26
Dima Blinov <gitportfoliocode@gmail.com>
2024-04-03 17:47:48 +0300

    first commit

--- main.js
+++ main.js
@@ -10,27 +10,6 @@ const pool = mysql2.createPool({
 
 const app = express();
 
-// app.get("/", function (req, res) {
-//   pool.query("SELECT * FROM cars").then(function (data) {
-//     const carMas = data[0];
-//     res.send(`<DOCTYPE html>
-//             <html>
-//                 <body>
-//                     <ul>
-//                         ${carMas
-//                           .map(
-//                             (item) => `
-//                         <li>${item.id} ${item.car} ${item.year}</li><hr>
-//                         `
-//                           )
-//                           .join("")}
-//                     </ul>
-//                 </body>
-//             </html>
-//             `);
-//   });
-// });
-
 app.get("/", function (req, res) {
   pool.query("SELECT * FROM cars").then(function (data) {
     const carMas = data[0];
@@ -38,6 +17,9 @@ app.get("/", function (req, res) {
             <html>
             <head>
 <style>
+body{
+  padding:30px;
+}
 
 table {
   font-family: arial, sans-serif;
@@ -56,19 +38,36 @@ td, th {
 tr:nth-child(even) {
   background-color: #dddddd;
 }
+h1{
+  text-align: center;
+  font-size:40px;
+}
 a{
-  width: 150px;
+  padding: 10px;
+  margin:10px;
   font-size: 30px;
+  color:blue;
+  text-decoration: none;
+  border: 1px solid black;
+  border-radius:5px;
+}
+a:hover{
+  background:green;
+  color:white;
 }
 </style>
 </head>
                 <body>
-                <a href="http://localhost:5000/add">Форма добавления данных!</a>
+                <h1>Телефоная книга</h1>
+                <a href="http://localhost:5000/add">Добавить</a>
+                <a href="http://localhost:5000/del">Удалить по (id_user)</a>
+                <a href="http://localhost:5000/delAll">Очистить всё</a>
                 <table>
                 <tr>
-                  <th>ID</th>
+                  <th>№ строки</th>
                   <th>Name</th>
                   <th>Number</th>
+                  <th>ID_user</th>
                 </tr>
                   ${carMas
                     .map(
@@ -77,6 +76,7 @@ a{
                   <td>${item.id}</td> 
                   <td>${item.name}</td> 
                   <td>${item.num}</td>
+                  <td>${item.id_user}</td>
                   `
                     )
                     .join("")}
@@ -92,19 +92,80 @@ a{
 const urlencodedParser = express.urlencoded({ extended: false });
 
 app.get("/add", function (_, response) {
-  response.sendFile(__dirname + "/index.html");
+  response.sendFile(__dirname + "/add.html");
 });
 
 app.post("/add", urlencodedParser, async function (req, res) {
   if (!req.body) return res.sendStatus(400);
-  const { name, num } = req.body;
+  console.log(req.body);
+  const { name, num, id_user } = req.body;
   await pool.query("INSERT INTO cars SET ?", {
     name,
     num,
+    id_user,
   });
   res.redirect("/");
 });
 // ===============================================================
+app.get("/del", function (_, response) {
+  response.sendFile(__dirname + "/del.html");
+});
+
+app.post("/del", urlencodedParser, async function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  const { id_user } = req.body;
+  await pool.query("DELETE FROM cars WHERE id_user = ?", id_user);
+  res.redirect("/win");
+});
+// ===============================================================
+
+app.get("/win", function (req, res) {
+  res.send(`<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Сообщение</title>
+        <style>
+          .win {
+            width: 100%;
+            margin: 0 auto;
+          }
+          h1 {
+            text-align: center;
+          }
+          a {
+            padding: 10px;
+            font-size: 30px;
+            color: blue;
+            text-decoration: none;
+            border: 1px solid black;
+            border-radius: 5px;
+            position: relative;
+            left: 390px;
+            top: 50px;
+          }
+          a:hover {
+            background: green;
+            color: white;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="win">
+          <h1>Удаление прошло успешно!</h1>
+          <a href="/">Вернуться к базе данных!</a>
+        </div>
+      </body>
+    </html>
+    
+            `);
+});
+
+app.get("/delAll", async function (req, res) {
+  await pool.query("TRUNCATE TABLE cars");
+  res.redirect("/");
+});
 
 app.listen(5000, function () {
   console.log("server started!");
---
+++ nodejs.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 01 2024 г., 16:54
+-- Версия сервера: 8.0.19
+-- Версия PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `nodejs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `cars`
+--
+
+CREATE TABLE `cars` (
+  `id` tinyint NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `num` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_user` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `cars`
+--
+
+INSERT INTO `cars` (`id`, `name`, `num`, `id_user`) VALUES
+(1, 'Дмитрий', '26218', 'ww2');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` tinyint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
